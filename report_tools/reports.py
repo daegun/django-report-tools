521cefa61103166c5aea40586549d95aadbc561d
David Lee <daegun@eatwith.com>
2015-07-30 16:38:18 -0700

    removing StrAndUnicode import as it has been deprecated

--- report_tools/reports.py
+++ report_tools/reports.py
@@ -1,13 +1,20 @@
 from copy import deepcopy
 
 from django.db import models
+from django.utils import six
 from django.utils.datastructures import SortedDict
-from django.utils.encoding import StrAndUnicode
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.safestring import mark_safe
 
 from .charts import Chart
 
 __all__ = ('BaseReport', 'Report')
+
+
+@python_2_unicode_compatible
+class StrAndUnicode(object):
+    def __str__(self):
+        return self.__unicode__()
 
 
 def pretty_name(name):
